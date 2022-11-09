c21c81bf86d89aab60382e3dfc2997ab62bf9beb
abdulwahab56 <108251096+abdulwahab56@users.noreply.github.com>
2022-11-09 10:17:41 +0500

    counter app using redux

---
+++ src/feature/counter/counterSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  count: 0,
+};
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count++;
+    },
+    decrement: (state) => {
+      state.count--;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+    incrementByAmount: (state, action) => {
+      state.count += action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, reset, incrementByAmount } =
+  counterSlice.actions;
+export default counterSlice.reducer;
